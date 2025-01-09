bff66c5d5e9a72d3f624df7c2c4ef9f18cb2ffce
Noa <JeuDeNoa@gmail.com>
2025-01-09 06:45:30 +0100

    debut page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Showdown from './components/Showdown'
+import AnimeList from './components/AnimeList'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/showdown" element={<Showdown />} />
+      <Route path="/anime" element={<AnimeList />} />
     </Routes>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import Navbar from './Navbar'; // Remplacez le chemin par celui de votre Navbar
+import UpcomingAnimeList from './UpcomingAnimeList';
+import NewAnimeList from './NewAnimeList';
+import './Home.css';
 function Home() {
   return (
     <div>
       {/* Votre contenu de page d'accueil actuel */}
-      <h1>Accueil</h1>
-      <Link to="/showdown">Aller au Showdown</Link>
+      <div>
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Contenu principal */}
+      <div style={{ display: 'flex', gap: '32px', padding: '16px' }}>
+        {/* Prochaines sorties */}
+        <div style={{ flex: 1 }}>
+          <h2>Prochaines Sorties</h2>
+          <UpcomingAnimeList />
+        </div>
+
+        {/* Nouveautés */}
+        <div style={{ flex: 1 }}>
+          <h2>Nouveautés</h2>
+          <NewAnimeList />
+        </div>
+      </div>
     </div>
+    </div>
+    
   )
 }
 
 export default Home
+
+
+
+
