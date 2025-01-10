6c83f5212b3572df14a9662436fa2ba6afb3dfcb
Noa <JeuDeNoa@gmail.com>
2025-01-10 06:27:18 +0100

    page login + mini css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Showdown from './components/Showdown'
 import AnimeList from './components/AnimeList'
+import LoginPage from './components/LoginPage'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/showdown" element={<Showdown />} />
       <Route path="/anime" element={<AnimeList />} />
+      <Route path="/login" element={<LoginPage />} />
+
     </Routes>
   )
 }
