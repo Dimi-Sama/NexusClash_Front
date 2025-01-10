b7e7ed68b71cfd84be707d47ee9544afa60ecc62
Dimi-Sama <dimitry97490@gmail.com>
2025-01-10 16:04:39 +0400

    live chat + battle + bg

--- src/components/Showdown.jsx
+++ src/components/Showdown.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './Showdown.css'
 import Navbar from './Navbar'
+import io from 'socket.io-client';
 
 function Showdown() {
   const [bataille, setBataille] = useState(null);
@@ -8,56 +9,167 @@ function Showdown() {
   const [tours, setTours] = useState([]);
   const [currentTour, setCurrentTour] = useState(0);
   const [error, setError] = useState(null);
+  const [socket, setSocket] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
+  const [backgroundLayers, setBackgroundLayers] = useState({
+    layer1: Math.floor(Math.random() * 328),
+    layer2: Math.floor(Math.random() * 328)
+  });
 
   useEffect(() => {
-    fetchBataille();
-  }, []);
+    // Connexion WebSocket
+    const newSocket = io('http://127.0.0.1:5000', {
+        withCredentials: false,
+        transports: ['websocket', 'polling'],
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
+    setSocket(newSocket);
 
-  const fetchBataille = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('http://127.0.0.1:5000/batailles', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: 'data='
+    // Demander la bataille actuelle au serveur
+    newSocket.emit('get_current_battle');
+
+    newSocket.on('current_battle', (data) => {
+        console.log('Bataille actuelle reçue:', data);
+        if (data) {
+            setBataille(data);
+            setLoading(false);
+            
+            // Rejoindre la room de la bataille
+            if (data.id_bataille) {
+                newSocket.emit('join_battle', { battle_id: data.id_bataille });
+                
+                // Demander l'historique des messages
+                newSocket.emit('get_message_history', { battle_id: data.id_bataille });
+            }
+        }
+    });
+
+    // Recevoir l'historique des messages
+    newSocket.on('message_history', (messages) => {
+        console.log('Historique des messages reçu:', messages);
+        setMessages(messages);
+    });
+
+    // Logs de débogage améliorés
+    newSocket.on('connect', () => {
+        console.log('Connecté au serveur WebSocket', newSocket.id);
+    });
+
+    newSocket.on('connect_error', (error) => {
+        console.error('Erreur de connexion:', error);
+    });
+
+    newSocket.on('nouvelle_bataille', (data) => {
+      console.log('Données bataille reçues:', data);
+      setBataille(data);
+      setLoading(false);
+      
+      // Mise à jour du fond lors de la réception d'une nouvelle bataille
+      setBackgroundLayers({
+        layer1: Math.floor(Math.random() * 328),
+        layer2: Math.floor(Math.random() * 328)
       });
       
-      if (!response.ok) {
-        throw new Error(`Erreur ${response.status}: Impossible de charger la bataille`);
+      // Rejoindre la room de la bataille
+      if (data && data.id_bataille) {
+          newSocket.emit('join_battle', { battle_id: data.id_bataille });
+          console.log('Rejoint la bataille:', data.id_bataille);
       }
-
-      const data = await response.json();
-      setBataille(data);
       
       if (data && data.resultat) {
         try {
-          const cleanResultat = data.resultat.replace(/\n/g, '\\n');
-          const resultatParse = JSON.parse(cleanResultat);
-          const toursArray = Object.values(resultatParse);
+          let toursData;
+          if (typeof data.resultat === 'string') {
+            // Si c'est une chaîne JSON valide, on la parse
+            try {
+              toursData = JSON.parse(data.resultat);
+            } catch {
+              // Si le parsing échoue, on utilise la chaîne directement
+              toursData = data.resultat;
+            }
+          } else {
+            // Si ce n'est pas une chaîne, on utilise directement
+            toursData = data.resultat;
+          }
+          
+          // Conversion en tableau si nécessaire
+          const toursArray = Array.isArray(toursData) 
+            ? toursData 
+            : typeof toursData === 'object'
+              ? Object.values(toursData)
+              : [String(toursData)];
+              
           setTours(toursArray);
-        } catch (parseError) {
-          console.error('Erreur de parsing:', parseError);
-          setTours([data.resultat]);
+        } catch (error) {
+          console.error('Erreur de traitement:', error);
+          setTours([String(data.resultat)]);
         }
       }
-      
-    } catch (error) {
-      console.error('Erreur:', error);
-      setError(error.message);
-      setBataille(null);
-    } finally {
-      setLoading(false);
+    });
+
+    newSocket.on('nouveau_message', (message) => {
+        try {
+            console.log('Message brut reçu:', message);
+            const messageData = typeof message === 'string' ? JSON.parse(message) : message;
+            console.log('Message traité:', messageData);
+            
+            if (messageData && messageData.contenu) {
+                setMessages(prevMessages => [...prevMessages, messageData]);
+            } else {
+                console.error('Format de message invalide:', messageData);
+            }
+        } catch (error) {
+            console.error('Erreur de traitement du message:', error, message);
+        }
+    });
+
+    newSocket.on('erreur_message', (error) => {
+        console.error('Erreur de message:', error);
+    });
+
+    // Nettoyage à la déconnexion
+    return () => {
+        console.log('Déconnexion du socket');
+        newSocket.disconnect();
+    };
+  }, []);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    if (messageInput.trim() && socket && bataille) {
+        const messageData = {
+            id_utilisateur: 1, // À remplacer par l'ID réel de l'utilisateur
+            id_bataille: bataille.id_bataille,
+            contenu: messageInput
+        };
+        
+        socket.emit('envoyer_message', messageData);
+        setMessageInput('');
+    }
+  };
+
+  // Modification de fetchBataille pour utiliser les websockets
+  const fetchBataille = () => {
+    if (socket) {
+        socket.emit('demander_bataille');
     }
   };
 
   return (
     <div className="showdown-page">
       <Navbar />
-      <main className="showdown-container">
+      <div className="showdown-container">
+        <div className="battle-background">
+          <iframe 
+            src={`https://www.gjtorikian.com/Earthbound-Battle-Backgrounds-JS/?layer1=${backgroundLayers.layer1}&layer2=${backgroundLayers.layer2}&fullscreen=true`} 
+            frameborder="0">
+          </iframe>
+        </div>
+
         <button className="new-battle-btn" onClick={fetchBataille}>
           Nouvelle Bataille
         </button>
@@ -92,30 +204,39 @@ function Showdown() {
             </div>
             
             <div className="battle-text">
-              {loading ? (
-                <p>Chargement du combat...</p>
-              ) : (
-                <p>{tours[currentTour]}</p>
-              )}
+                {loading ? (
+                    <p>Chargement du combat...</p>
+                ) : bataille && bataille.resultat ? (
+                    <p>{bataille.resultat}</p>
+                ) : (
+                    <p>En attente d'une nouvelle bataille...</p>
+                )}
             </div>
           </>
         )}
-      </main>
+      </div>
 
       <div className="chat-section">
         <div className="chat-messages">
-          <div className="chat-message">
-            <span className="username">User1:</span>
-            <span className="message">Message text</span>
-          </div>
+          {messages.map((msg, index) => (
+            <div key={index} className="chat-message">
+              <span className="username">Utilisateur {msg.id_utilisateur}:</span>
+              <span className="message">{msg.contenu}</span>
+            </div>
+          ))}
         </div>
         <div className="chat-input-container">
           <input 
             type="text" 
-            placeholder="Send a message..." 
+            value={messageInput}
+            onChange={(e) => setMessageInput(e.target.value)}
+            placeholder="Envoyez un message..." 
             className="chat-input"
+            onKeyPress={(e) => e.key === 'Enter' && handleSendMessage(e)}
           />
-          <button className="chat-send-button">Send</button>
+          <button className="chat-send-button" onClick={handleSendMessage}>
+            Envoyer
+          </button>
         </div>
       </div>
     </div>
@@ -123,3 +244,39 @@ function Showdown() {
 }
 
 export default Showdown 
+// Dimi-Sama est passé par ici le 10/01/2025
+// ⢌⢣⡝⡼⣙⢮⢳⠞⣦⢣⠄⠀⠄⡀⢀⠀⢆⡐⢢⠐⡄⢢⠐⢢⠐⠤⠐⢂⠐⠀⠀⡠⠄⢂⠰⡀⢆⠰⡀⢆⠰⣀⠒⡄⢒⡐⢂⡒⠰⢂⠲⢄⠣⢆⡱⠢⢜⢢⡑⢎⢆⢣⠜⣢⠹⣌⡳⡝⣮⣝
+// ⠀⢢⠘⡰⢉⡎⢯⡞⣥⢏⠄⡈⠐⠀⠀⡈⠄⠘⠆⠣⠜⣠⠉⠆⠀⠀⠀⠈⠄⠀⠀⠁⠈⠀⠁⡈⠀⠃⠘⠤⠁⢆⠱⢈⠆⡌⡡⠜⣡⠋⢦⢉⠆⡣⠜⣑⠪⡰⢘⡌⢎⠦⡹⢆⡳⢌⢳⡙⣖⢺
+// ⠐⡀⢆⠡⢳⡘⡧⢞⡱⡞⠀⠄⡁⠀⠀⠐⡈⠄⡀⠀⠀⠀⠉⠀⠀⠀⠀⡀⠀⠈⣔⠪⡜⣩⠣⡍⣍⢣⢓⡒⠲⡤⠬⣄⠊⠔⡡⢚⠤⡙⢤⣺⠼⡐⣍⢢⠱⣁⠣⢜⡨⢒⡍⢲⡉⢞⢢⡕⢪⡱
+// ⡐⠰⢀⠣⢡⠹⣜⢫⡵⡋⠐⠠⠄⠀⠀⠀⠐⠀⠀⠀⠀⠀⠄⠀⢀⠀⠁⡀⢀⠀⠈⢳⡘⢤⡓⡜⠤⠓⡎⣜⠳⣌⠳⣌⡹⢒⠦⣁⠶⡉⢦⡑⢎⡱⢌⢆⠳⣌⠱⢢⢅⠣⡜⡡⢎⡱⢊⡼⢡⡓
+// ⡌⠱⡈⠔⡁⠎⡐⢣⠳⡅⢈⡐⠄⡀⠀⠀⠀⠀⢀⠀⠀⣀⠤⡒⢬⣉⠧⠙⠌⠋⠲⢄⡙⢦⡱⢜⡱⢢⠈⢀⡉⣀⡙⡰⢭⡙⡼⡑⠶⣌⠦⡑⢎⠴⣉⢎⡱⢌⢣⠣⢎⡱⢢⡑⢎⡔⠫⣐⢣⠘
+// ⢍⡣⢝⡸⡑⢎⠔⢣⣙⠀⠂⠐⠀⠀⠀⠀⠂⠁⢀⡠⢚⠤⢣⡙⢢⠔⣢⠝⡬⢋⡵⢪⠜⠆⢳⣊⡱⣉⡌⣡⢈⣀⠑⠓⢦⡙⣖⣩⠳⣌⠞⣱⠪⡔⠣⢎⡔⢫⠬⣙⢎⡱⢣⠙⡢⢌⡱⢂⢎⠱
+// ⣪⢔⡣⡒⡍⢎⡸⢤⠌⠀⠀⠀⠀⠀⠈⠀⢀⡰⢊⡔⡩⢎⡱⢜⢣⡙⢆⡹⢜⢣⣚⡱⢎⡝⢦⡀⠳⡜⡼⣡⢏⡼⣙⢎⠦⣜⠢⡕⡳⡜⢺⢤⡑⠩⣓⠌⠘⡥⠚⠔⡊⠴⣁⠫⢔⠣⡜⡡⣊⠮
+// ⣛⢮⡵⣹⢞⣧⣛⠇⠀⠀⠀⠀⠀⠐⠀⡤⠃⡰⢡⢎⡱⣊⡕⠮⡅⠁⢠⢏⢎⠇⠈⡞⡼⣘⢧⡹⢄⠻⣔⢣⠞⡴⣩⢎⡳⣌⠳⣌⢣⢝⡣⠞⣜⡄⠈⠙⠆⡈⠔⡡⢌⠱⢠⠋⣌⠱⡘⡔⢣⠳
+// ⣿⣹⣞⡽⣞⣮⡝⠀⠀⠀⠀⡀⠈⢀⠎⠀⡴⣉⠖⡎⡴⢣⡜⠃⠀⠀⡸⠎⠊⠀⠀⡟⡴⣍⠶⣙⠮⡜⡬⢧⡛⣤⢓⢮⡱⢎⡳⣜⢢⠣⣹⡙⢦⡙⠢⡘⢢⠉⠂⡅⢢⠘⡠⢑⠠⢃⡜⣌⢣⠓
+// ⣷⣳⣾⣽⠮⠓⠠⠀⠀⠀⠀⠀⠠⠃⠀⡜⠴⢉⡜⣜⢱⡃⢢⢀⠠⠀⠝⠀⠀⢀⠂⡽⢲⡍⡞⣥⢛⡰⣝⢲⡙⢦⡋⢖⡹⢎⡵⣊⢧⢃⠡⣛⠄⢀⠱⠈⠀⢄⠓⡬⢡⢎⡰⢁⠢⡅⡒⠌⠦⣉
+// ⣷⢫⡿⣿⣦⠁⠂⠀⠀⠀⡀⠄⠁⠀⡸⢌⠁⡖⠼⣌⠓⡠⠃⣬⣶⣀⣠⢂⡈⠀⢸⣍⢳⡜⣣⢇⢯⠐⣎⢧⡹⢣⣝⠨⡳⣍⠶⣩⠆⢫⠆⠘⠄⠀⠁⠀⡈⢤⠋⡔⢣⢊⠴⣋⠐⠈⠱⣉⠖⡡
+// ⡾⣯⢽⣞⠾⠀⠀⠀⠀⠀⠀⠀⠀⢠⠣⠁⠰⣘⠣⢎⠐⣡⣽⣿⣿⣿⣿⣿⡏⠱⡸⢬⣓⠮⡵⣚⠮⢘⡜⢮⣱⢫⢜⡣⢵⣊⠷⣡⢏⠀⠷⠀⠈⠀⠀⠀⡜⠠⡍⢬⡑⢎⡒⣬⢛⢦⡐⠈⡖⡡
+// ⡿⢧⣟⡾⡃⠀⠀⠀⠀⠀⠀⠀⠀⠰⠁⠀⣸⠁⠎⡄⢃⣾⣿⣿⣿⣿⣿⣿⠇⡁⡟⣧⡚⣝⡲⣍⠇⠸⣜⠳⣜⢣⢸⡱⡘⣬⢓⠧⣞⡀⠈⠇⠀⠀⠠⢀⠣⡑⠌⠂⡘⠠⠈⢵⣋⡞⡴⡀⠘⡥
+// ⣯⣟⠾⣽⠁⠀⠀⠀⠀⠀⠀⠀⠠⢁⠠⠀⡄⠈⡔⣈⣾⣿⣿⣿⣿⣿⣿⡿⣈⠴⣻⠴⣹⢲⡹⡌⠀⢸⢎⡽⢬⡓⢨⢳⡁⢮⡝⡺⣔⡃⠀⠘⠀⠀⠀⠀⠀⠀⢀⠘⣭⢓⡌⠰⣍⢞⡱⢇⠀⢋
+// ⣷⢞⡻⣣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠐⠀⠀⢢⠐⣾⣿⣿⣿⣿⣿⣿⣿⠇⡄⣛⢦⡋⢧⠇⡝⠀⠀⣏⡞⡼⢣⠇⢘⢧⠂⢣⢏⡵⢎⡅⠀⠀⠀⠂⢈⡑⠢⣀⠀⠀⢧⣋⠶⡘⡜⢮⡱⢫⠔⠀
+// ⣯⢋⣴⡇⠀⠀⠀⡀⠀⠀⡀⠀⢁⠆⣰⢂⠌⠰⠠⣹⣿⣿⣿⣿⣿⣿⡟⢨⢰⢫⠖⣩⢳⠘⠀⠀⠸⣜⠼⣹⡍⠂⢸⢎⠁⢘⠮⣜⡣⠄⠀⠀⠀⠀⠀⠆⡑⢠⠂⡄⢳⣌⠳⣥⠛⣦⡙⠧⠎⠀
+// ⣵⣻⣞⡇⠀⠀⠀⠀⠀⢀⠀⠀⡌⢰⢣⠋⠀⠀⠑⣺⣿⣿⣿⣿⣿⣿⢁⢃⡮⠇⢡⢇⠣⠀⠀⠂⢸⡜⣣⠗⠎⠀⢸⠎⠀⠈⡗⢮⡱⠀⠀⠀⠂⠀⢀⠘⠄⢣⠘⡠⢂⢪⠳⢬⡙⢦⠭⣙⠣⠀
+// ⣿⣵⢻⠆⠀⠀⠀⠀⠄⠀⠀⠀⡜⢬⡳⣤⠆⡥⢸⣿⣿⣿⣿⣿⣿⠇⡌⡼⠘⢀⡎⣡⠂⠀⠁⢈⠶⣙⠦⠋⠀⠌⣸⠃⠀⠀⣏⠇⡇⠀⠀⠠⠀⠀⠀⠎⠘⠄⠣⠐⡁⢂⠹⣰⡙⣎⠧⣍⠧⠀
+// ⡿⣞⡿⠀⠀⠀⠀⠂⠀⢀⠈⠰⡈⢶⢁⣾⡲⠇⣻⣿⣿⣿⣿⣿⡏⡰⠐⠀⡐⠀⢴⠡⢀⠀⢐⢊⡱⠣⠉⠀⡠⠃⡎⠀⠀⠀⣸⠙⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⣤⣀⠐⠂⡵⢚⡜⡲⢜⡢⢤
+// ⢽⡺⡕⠀⠀⠠⠁⠀⠀⡀⠀⡱⢈⠆⡌⢡⠉⢴⣿⣿⣿⣿⣿⡟⣰⠏⣠⠎⣀⠼⠀⠃⠄⢠⡏⠰⠌⠁⠠⣰⠇⡑⠀⣄⠂⠁⢸⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠈⢰⢣⡜⡍⢰⣍⠳⡜⢥⢫⡄⣿
+// ⠎⠷⠃⠀⢀⠆⠀⠀⢀⠀⠀⡱⢈⡒⠌⡆⣉⣾⣿⣿⣿⣿⣿⣾⣿⣿⣵⣷⣭⣶⣻⣝⠒⠀⠀⢀⠀⠃⣼⣿⢘⠄⣼⣃⠀⠀⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⣏⠶⡉⠀⢶⣨⢓⠭⣎⡱⠂⢿
+// ⢎⡱⠀⠀⡌⠀⠀⠀⠀⠀⠄⡑⢢⠑⣊⢴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣾⣳⠀⡀⠀⠄⡈⢏⣞⣼⡯⠄⠀⠈⠀⢀⠈⠀⠀⠀⠀⠄⠀⠄⠀⢰⡍⡞⠀⢸⣃⠶⡩⠞⣤⠓⠈⡄
+// ⣿⠀⠀⡘⠀⠀⠀⠀⠁⠀⠠⠘⡄⣃⠆⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣸⣷⣧⣴⣛⡆⡰⢉⣴⣿⣿⣿⠔⠀⢁⠠⠀⠀⢀⠈⠀⠀⢀⠠⠀⢀⠷⡘⠀⢠⠳⣌⢳⣉⢳⠰⠃⠀⢂
+// ⠃⠀⡰⠁⠀⠀⢀⠈⠀⠀⢁⠠⡘⢄⢊⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣮⣜⣛⣶⣭⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠏⢴⣿⠄⠀⠀⠈⠀⠀⠀⣎⠃⠀⢀⣎⠳⣌⠧⡜⡁⡚⠀⠀⠠
+// ⠀⠰⠁⠠⠀⠀⠀⠀⠀⠈⠀⢀⠈⢆⠣⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⢀⣆⡂⣿⠀⠀⠀⠁⠀⠀⡜⠀⠀⢠⠞⣌⢳⢸⡘⠆⢠⠁⠀⠀⣼
+// ⠀⡱⢀⠃⠀⠀⡀⠁⠀⠐⠈⠀⡀⢈⠒⡌⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠁⠄⣻⣿⣽⡿⠀⠀⠀⠂⠁⠈⠀⢀⢠⡓⢞⡌⢧⣊⠵⠀⠄⢠⠀⢻⡽
+// ⠠⠁⡄⡃⠀⠀⠀⠀⠀⠂⡀⠠⠀⠀⠣⠌⣿⣿⣿⣯⣿⣻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⣀⣎⣴⣿⣿⡿⠁⠀⠀⠀⡀⠄⠀⢀⡴⢣⡙⣎⡜⢦⡱⠀⠀⢠⠣⡄⠀⢻
+// ⢆⠡⢂⠥⠀⠀⠁⠀⢀⠀⣇⠀⠠⠐⠀⢃⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⢋⠅⡈⠛⠃⠶⡽⠷⠾⠿⠛⠁⠀⠀⠀⢀⠀⠀⣰⢊⡖⡣⣝⡰⡚⡔⠀⠀⠀⣎⠱⡘⣄⠀
+// ⣊⠰⣉⠒⠀⠀⡀⠁⠀⢨⡽⡀⠀⠀⠐⠈⠼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣟⠛⡏⢍⡒⡘⡄⢣⠘⠤⠡⠌⠄⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⣤⠚⣥⠳⣸⢑⠦⢣⠍⠀⠀⠀⠼⣐⢣⠱⡌⠂
+// ⢆⠰⢂⡍⠂⠀⠀⠀⠀⢸⣳⣟⡀⠀⠀⠂⠉⢁⠉⢉⠉⡉⢉⠉⣘⡐⢢⠥⠣⢌⠚⡌⠒⡌⠰⡘⠤⡉⣠⠇⡤⣀⡀⠀⠀⠀⠀⡀⠁⣠⢔⡫⢆⡛⣤⠳⣡⠎⡝⠁⠀⠀⠐⠀⠠⢑⠊⡕⠈⠀
+// ⢎⡐⠣⢌⡅⠀⠀⠁⠀⢸⣷⣫⣟⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⡐⢌⠡⢊⠱⣈⠒⡌⠱⣈⠱⣰⠟⢩⢆⡹⢔⡡⠆⠀⠀⠂⢠⢔⡹⣂⠷⣨⢇⡙⢶⣿⣶⣍⠀⠄⠀⠁⠀⡱⢄⡀⠑⠌⠀⠀
+// ⠀⠬⡑⠢⢜⡀⠠⠀⠁⠈⢷⣟⣾⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⠀⠈⢢⢁⠒⡄⢃⠌⠡⠐⢠⡒⣌⠣⢎⡔⢣⠌⠀⣀⠴⣩⠓⣎⡱⢜⠢⢇⢎⡹⢌⠝⠛⠊⠁⠀⠀⠐⠀⠘⠢⢍⠒⡀⠀⠀
+// ⡁⠠⠘⡑⢢⠜⡀⠀⠄⠂⠀⠙⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡀⠀⠁⠈⠀⠀⠀⡰⢣⠐⣌⠳⢬⠘⢀⠤⣪⠕⣣⢃⢯⠐⡡⢎⡝⡸⢌⡒⠁⡀⢤⠰⢌⡒⠤⢄⡀⠄⠈⠊⡑⠀⠀⠀
+// ⣷⡄⠀⠀⠡⢊⠱⠄⢀⠀⠢⡀⠀⠳⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⣡⠃⡘⢦⠙⢀⠴⣩⠚⣥⠚⡥⠋⠀⡰⢱⠪⠴⢉⠤⡰⠩⠜⣄⠫⢰⠘⣘⠢⡘⡐⢢⠀⠀⠀⠀⠀
+// ⣧⠉⢲⣀⠀⠠⠑⢊⠤⠀⠄⢑⠢⠀⠀⠄⠀⠀⢀⠀⢀⠠⣀⢀⠀⠀⠀⠀⠀⠀⠀⡜⡱⢂⠇⡩⠂⢤⣋⡜⢆⡻⢰⡙⠂⠀⢡⢍⠣⢉⡔⡡⢎⢡⠣⠍⣄⠣⢡⠚⢄⠣⡐⢡⠂⠀⠀⠀⠀⠀
